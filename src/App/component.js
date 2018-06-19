4da7712292193b9485a90d43fd0fef1457f08393
Ceci <ceci.goss@gmail.com>
2018-06-19 04:37:48 -0500

    i think i'm done??

--- src/App/component.js
+++ src/App/component.js
@@ -73,6 +73,9 @@ class AppComponent extends React.Component {
             <TrendViewer selectedLetters={selectedLetters} />
           </div>
         </div>
+        <div className="footer">
+          please hire me
+        </div>
       </div>
     );
   }
