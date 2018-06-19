26fb67f7829b4f47c68540fee88ecbbbc38a557b
Ceci <ceci.goss@gmail.com>
2018-06-19 10:46:17 -0500

    modified banner text

--- src/components/Banner/component.js
+++ src/components/Banner/component.js
@@ -9,7 +9,7 @@ const BannerComponent = () => {
       <div className="title">Trending Letters Helper Tool 🤔</div>
       <div className="web-info">
         <p>
-          Inspired by viewers like you. <a href="http://ceci.io/">ceci.io</a>
+          Made to accompany this <a href="https://medium.com/@cc21/top-10-letters-of-the-alphabet-that-you-should-pay-attention-to-and-why-i-decided-e-is-superior-to-79bccf8b1206">Medium article.</a> Inspired by viewers like you. <a href="http://ceci.io/">ceci.io</a>
         </p>
       </div>
     </div>
